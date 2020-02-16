7ad4cf8e0cda8f986ce1e48e0cc2cb3f3401857b
Jester0027 <Paul.NED@outlook.com>
2020-02-16 17:57:51 +0100

    :white_check_mark: Added event fixtures

---
+++ src/DataFixtures/EventFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use App\Entity\EventTheme;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class EventFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create("fr_BE");
+
+        $numberOfThemes = 5;
+        $themes = [];
+
+        for($i = 0; $i < $numberOfThemes; $i++) {
+            $theme = new EventTheme();
+            $theme->setName($faker->catchPhrase())
+                ->setDescription($faker->realText())
+            ;
+            $manager->persist($theme);
+            array_push($themes, $theme);
+        }
+
+        for($i = 0; $i < 20; $i++) {
+            $event = new Event();
+            $latitude = $faker->latitude();
+            $longitude = $faker->longitude();
+            $event->setTitle($faker->text(50))
+                ->setEventDate($faker->dateTimeBetween('now', '6 months', 'Europe/Paris'))
+                ->setAddress($faker->streetAddress())
+                ->setCity($faker->city())
+                ->setZipCode((int)$faker->postcode())
+                ->setCoordinates("$latitude , $longitude")
+                ->setImageUrl($faker->imageUrl())
+                ->setDescription($faker->realText())
+                ->setEventTheme($themes[rand(0, $numberOfThemes - 1)])
+            ;
+            $manager->persist($event);
+        }
+
+        $manager->flush();
+    }
+}

581fe3eb27349c9c4caa1d38eeb88389ea0ed117
Jester0027 <Paul.NED@outlook.com>
2020-02-20 20:43:42 +0100

    :bug: MethodNotAllowed exception handler

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionSubscriber implements EventSubscriberInterface
@@ -13,7 +14,11 @@ class ExceptionSubscriber implements EventSubscriberInterface
     {
         $exception = $event->getException();
         if ($exception instanceof NotFoundHttpException) {
-            $data = ["error" => 404, "message" => "resource not found"];
+            $data = ["errorCode" => $exception->getStatusCode(), "message" => "resource not found"];
+            $response = new JsonResponse($data, 404);
+            $event->setResponse($response);
+        } else if ($exception instanceof MethodNotAllowedHttpException) {
+            $data = ["errorCode" => $exception->getStatusCode(), "message" => "method not allowed"];
             $response = new JsonResponse($data, 404);
             $event->setResponse($response);
         }

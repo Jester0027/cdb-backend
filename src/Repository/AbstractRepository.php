01ce6b09d020c750dc77c31c86d4f1685a236705
Jester0027 <Paul.NED@outlook.com>
2020-03-15 17:47:26 +0100

    Added abstract repository for paginate method

---
+++ src/Repository/AbstractRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use Pagerfanta\Pagerfanta;
+use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+abstract class AbstractRepository extends ServiceEntityRepository
+{
+    protected function paginate(QueryBuilder $qb, $limit = 10, $currentPage = 1)
+    {
+        if (0 >= $limit || 0 >= $currentPage) {
+            throw new \LogicException('$limit & $offset must be greater than 0.');
+        }
+
+        $pager = new Pagerfanta(new DoctrineORMAdapter($qb));
+        $pager->setCurrentPage($currentPage);
+        $pager->setMaxPerPage((int) $limit);
+
+        return $pager;
+    }
+}

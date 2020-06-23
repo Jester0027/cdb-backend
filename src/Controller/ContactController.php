fa216c8c413ce8d7d3025d8c3702f7d9ae337d5e
Jester0027 <Paul.NED@outlook.com>
2020-06-24 00:24:25 +0200

    Recaptcha server-side implementation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Mailer\MailerService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 /**
  * @Route("/contact")
@@ -17,11 +22,32 @@ class ContactController extends AbstractController
      * @Route("/send_contact", name="send_contact", methods={"POST"})
      *
      * @param MailerService $mailer
+     * @param Request $request
+     * @param HttpClientInterface $client
      * @return JsonResponse
      * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
-    public function sendContact(MailerService $mailer): JsonResponse
+    public function sendContact(MailerService $mailer, Request $request, HttpClientInterface $client): JsonResponse
     {
-        return $mailer->sendContactDemand();
+        $data = json_decode($request->getContent());
+        $userKey = $data->userKey;
+        $sharedKey = $_ENV['G_SECRET'];
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+        $res = $client->request('POST', $url, [
+            'body' => [
+                'secret' => $sharedKey,
+                'response' => $userKey
+            ]
+        ])->getContent();
+        $data = json_decode($res);
+
+        if ($data->success) {
+            return $mailer->sendContactDemand();
+        }
+        return new JsonResponse(['code' => 500, 'message' => 'INVALID_CAPTCHA'], 500);
     }
 }
